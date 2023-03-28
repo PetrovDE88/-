e7a37b2580177230a1c423c2e968e9b354e65327
Dmitry <114431288+PetrovDE88@users.noreply.github.com>
2023-03-28 09:18:04 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+from collections import Counter
+
+
+def can_be_poly(string: str) -> bool:
+    return len(list(filter(lambda x: x % 2, Counter(string).values()))) <= 2
+
+
+if __name__ == '__main__':
+    print(can_be_poly('abcba'))
+    print(can_be_poly('abbbc'))
